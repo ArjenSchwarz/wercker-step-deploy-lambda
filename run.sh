cae0611e87323aff5df4067e422a451e6be90007
Arjen Schwarz <developer@arjen.eu>
2016-04-21 17:55:54 +1000

    Add run.sh code and wercker-step config

--- run.sh
+++ run.sh
@@ -1,1 +1,25 @@
 #!/usr/bin/env bash
+
+export AWS_ACCESS_KEY_ID=$WERCKER_LAMBDA_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$WERCKER_LAMBDA_SECRET_KEY
+
+PUBLISH=""
+if [[ $WERCKER_LAMBDA_PUBLISH == "false" ]]; then
+  $PUBLISH="--publish false"
+fi
+
+if [[ -z $WERCKER_LAMBDA_REGION ]]; then
+  WERCKER_LAMBDA_REGION="us-east-1"
+fi
+
+LAMBDA="${WERCKER_STEP_ROOT}/lambda-deploy --functionname ${WERCKER_LAMBDA_FUNCTION_NAME} --region ${WERCKER_LAMBDA_REGION} --filepath ${WERCKER_LAMBDA_FILEPATH} ${PUBLISH}"
+debug "$LAMBDA"
+update_output=$($LAMBDA)
+
+if [[ $? -ne 0 ]];then
+    echo "${update_output}"
+    fail 'Lambda update failed';
+else
+    echo "${update_output}"
+    success 'Lambda update succeeded';
+fi
